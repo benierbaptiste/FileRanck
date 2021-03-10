1eae5d71e9759a7c64e363f3d100e01f29378a7b
miisthydralisk <79928883+miisthydralisk@users.noreply.github.com>
2021-03-10 11:50:51 +0100

    recherche barre 2
    oui

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
+    <head><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
@@ -16,23 +17,24 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+		
+
+	
     </head>
      
-    <body>
-	   	<?php
-	 
-	try $bdd = new PDO('mysql:host=localhost;dbname=filerank;charset=utf8','root','');
-	$bdd->setAttribute(pdo::attr_errmode, PDO::ERRMODE_EXCEPTION);
-	catch(exception $e) die ("Une erreur a été trouvée :" .$e->getmessage());
-	$Ids = $bdd->query('SELECT TitreDoc FROM Document ORDER BY ASC'); 
-		?>
-    <form action = "verif-form.php" method="GET">
-	   <input type="search" name="q" placeholder="Recherche..." />
-	   <input type="submit" value="Rechercher" />
-	</form>
-
-<button>rechercher</button>
-
+    <body>	
+	<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+		<a class="navbar-brand js-scroll-trigger" href="#page-top">Fileranck</a>
+	
+		<div class="form-group"href="#page-top">
+		<input class="form-control" type="text" value="" placeholder="rechercher un ou plusieurs documents">
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<button type="button" class="btn btn-secondary">Rechercher</button>
                 <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
