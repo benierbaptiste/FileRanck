373c7f22e0fbf125bec384d97c30c9eebbb0fcff
benierbaptiste <60742786+benierbaptiste@users.noreply.github.com>
2021-04-02 10:45:14 +0200

    créer et liste
    créer et liste

--- views/theme/list.php
+++ views/theme/list.php
@@ -1,44 +1,26 @@
 <body>
-<?php
-try
-{
-$connexion = new PDO('mysql:host=localhost;dbname=filranck;charset=utf8', 'root', '');
-$reponse = $connexion->query("SELECT * FROM theme");
-$lesThemes = $reponse->fetchAll();
-$connexion = NULL; 
-}
-    catch (PDOException $e)
-
-	{
-		include "views/404.php";
-	}
-    ?>
-    <?php
-    $str = $lesThemes[$i]["description"];
-    ?>
-        <table>
-            <tr>
-                <td><?php echo $lesThemes[$i]["NomTheme"]?></td>
-                <td><?php echo $lesThemes[$i]["libelle"]?></td>
-                <td><?php
-                if (strlen($str)>20)
-                {
-                    $taille = strlen($str) - 20;
-                    echo $rest = substr($str, 0,-$taille); ?></td>
-                <?php
-                }
-                else
-                {
-                    echo $lesThemes[$i]["description"]?> </td>
-                <?php
-                }
-                for ($i=0; $i<count($lesThemes); $i++)
-                {
-            
-                ?>
-                
-            </tr>
-            </table>
-        
-        
+	<?php
+		try
+		{
+			$connexion = new PDO('mysql:host=localhost;dbname=filranck;charset=utf8', 'root', '');
+			$reponse = $connexion->query("SELECT * FROM theme ");
+			$lesTheme = $reponse->fetch(); 
+		}
+    	catch (PDOException $e)
+		{
+			include "views/404.php";
+		}
+		for($i=0; $i<count($lesTheme); $i++)?>
+		<table>
+			<tr>
+			Titre Description 
+				<td>
+					<?php
+					echo $lesTheme['NomTheme'].' | ';
+					echo $lesTheme['DescriptionTheme'].' | ';
+					?>
+				</td>
+			</tr>
+		</table>
+		<?php $connexion = NULL; ?>    
 </body>
